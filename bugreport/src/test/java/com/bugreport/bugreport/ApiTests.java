e8e75a1d0e23838b3187a42783a2b9d26335b480
Maikel Hendrikx <464318@student.fontys.nl>
2022-04-27 16:13:33 +0200

    Database is now properly reset after each test.

--- bugreport/src/test/java/com/bugreport/bugreport/ApiTests.java
+++ bugreport/src/test/java/com/bugreport/bugreport/ApiTests.java
@@ -1,25 +1,35 @@
 package com.bugreport.bugreport;
 
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.util.Assert;
+
+import de.cronn.testutils.h2.H2Util;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Import(H2Util.class)
 public class ApiTests {
     @Autowired
     private MockMvc mockMvc;
@@ -31,7 +41,7 @@ public class ApiTests {
     private BugReportRepository bugReportRepository;
 
     @BeforeEach
-    public void ResetPersistence(){
+    public void InitializeEntities(){
         List<BugReport> initialEntities = List.of(
             new BugReport("Test report 1", "Test report 1 description"),
             new BugReport("Test report 2", "Test report 2 description"),
@@ -43,6 +53,11 @@ public class ApiTests {
         );
 
         bugReportRepository.saveAll(initialEntities);
+    }
+
+    @AfterEach
+    public void ResetTable(@Autowired H2Util h2Util){
+        h2Util.resetDatabase();
     }
 
     @Test
