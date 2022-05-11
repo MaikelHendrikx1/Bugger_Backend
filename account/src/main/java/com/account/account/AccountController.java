f831f6ec7984ad96100ee399369d3bd48d46438e
Maikel Hendrikx <464318@student.fontys.nl>
2022-05-11 09:42:43 +0200

    Register fix

--- account/src/main/java/com/account/account/AccountController.java
+++ account/src/main/java/com/account/account/AccountController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +15,7 @@ public class AccountController {
     @Autowired
     AccountService accountService;
 
-    @PostMapping(path = "/register")
+    @GetMapping(path = "/register")
     public @ResponseBody void addNewAccount(@RequestParam String email, @RequestParam String username, @RequestParam String password) {
         accountService.addNewAccount(username, email, password);
     }
